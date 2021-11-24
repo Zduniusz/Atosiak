723777db54db805eb712f3f0ff6bddb50569252d
Zduniusz <31253374+Zduniusz@users.noreply.github.com>
2021-11-24 09:05:30 +0100

    Revert "Fix Timezones and rewrite monitors command"

--- src/main/java/com/zduniusz/data/monitors/Monitors.java
+++
@@ -1,9 +0,0 @@
-package com.zduniusz.data.monitors;
-
-
-import java.util.List;
-
-public class Monitors {
-    public String[] date;
-    public List<String> monitors;
-}
--- src/main/java/com/zduniusz/discord/commandlist/LuckyNumber.java
+++ src/main/java/com/zduniusz/discord/commandlist/LuckyNumber.java
@@ -3,14 +3,12 @@ package com.zduniusz.discord.commandlist;
 import com.zduniusz.Main;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 
-import java.time.*;
-import java.time.temporal.TemporalAccessor;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.Optional;
 
 public class LuckyNumber {
     public static void reply(SlashCommandEvent event) {
-        LocalDate today = LocalDate.now(ZoneId.of("CET"));
+        LocalDate today = LocalDate.now();
 
         Optional<com.zduniusz.data.luckynumber.LuckyNumber> optionalLuckyNumbers = Main.luckyNumberList.stream().filter(x -> x.date.equals(today.toString().replace("-", "."))).findFirst();
 
