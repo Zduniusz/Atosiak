af734eb87d23c155d376c9ca1130372b1b135ce2
kabanos-2137 <tvoastara251@gmail.com>
2021-11-24 08:09:58 +0100

    Add thread to Main.java

--- src/main/java/com/zduniusz/Main.java
+++ src/main/java/com/zduniusz/Main.java
@@ -4,6 +4,8 @@ import com.zduniusz.data.covid.DailyCovidStat;
 import com.zduniusz.data.covid.Download;
 import com.zduniusz.data.luckynumber.GetData;
 import com.zduniusz.data.luckynumber.LuckyNumber;
+import com.zduniusz.data.monitors.GetMonitors;
+import com.zduniusz.data.monitors.Monitors;
 import com.zduniusz.discord.CommandManager;
 import com.zduniusz.discord.Setup;
 import net.dv8tion.jda.api.JDA;
@@ -18,14 +20,17 @@ public class Main {
 
     public static DailyCovidStat dailyCovidStat;
     public static List<LuckyNumber> luckyNumberList;
+    public static List<Monitors> monitorsList;
 
     public static void main(String[] args) throws LoginException, IOException, NoSuchMethodException {
         new Thread(new com.zduniusz.threads.UpdateCovid()).start();
         new Thread(new com.zduniusz.threads.UpdateLuckyNumber()).start();
+        new Thread(new com.zduniusz.threads.UpdateMonitors()).start();
 
 
         dailyCovidStat = Download.formatData(Download.downloadData());
         luckyNumberList = new GetData().getLuckyNumbersFromResources();
+        monitorsList = new GetMonitors().getMonitorsFromResources();
 
         Setup.setup();
 
